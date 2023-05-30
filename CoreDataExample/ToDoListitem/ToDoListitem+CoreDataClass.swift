c1f5dcc816758a25d9d58b8ce686d03863827eee
intern <intern@Puran-iMac.local>
2023-05-30 13:50:19 +0545

    core data

---
+++ CoreDataExample/ToDoListitem/ToDoListitem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListitem+CoreDataClass.swift
+//  CoreDataExample
+//
+//  Created by intern on 5/30/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListitem)
+public class ToDoListitem: NSManagedObject {
+
+}
